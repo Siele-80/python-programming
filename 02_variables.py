e596fe0aaa2e415e1ffb804fd8021ab29536cb81
Siele-80 <sharonsiele80@gmail.com>
2026-04-27 19:42:24 +0300

    Added all 5 Python programming examples

---
+++ 02_variables.py
@@ -0,0 +1,6 @@
+name = "Sharon"
+age = "22"
+subject = "Python"
+print("My name is" + name)
+print("I am" +str(age) + "years old")
+print("I am learning" + subject)
---
+++ 04_if_statements.py
@@ -0,0 +1,6 @@
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are adult pro!")
+else:
+    print("You are really young!")
